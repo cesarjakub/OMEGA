aef3f529d4c3fa41f0bd529519d5569f6536b5a7
cesarjakub <cesar@spsejecna.cz>
2024-04-14 15:50:16 +0200

    feat: new bug

--- src/presentations/import_file_scene.py
+++ src/presentations/import_file_scene.py
@@ -40,7 +40,7 @@ class ImportFileScene:
             import_data = ImportDAO(self.database)
 
             for imp in data:
-                import_data.import_data(imp['title'], imp['Author_first_name'], imp['Author_last_name'], imp['genre'], imp['publisher'], imp['shelf_nu'], imp['floor_nu'])
+                import_data.import_data(imp['title'], imp['Author_first_name'], imp['Author_last_name'], imp['genre'], imp['publisher'], imp['shelf_number'], imp['floor_number'])
 
             self.file_path = None
             self.selected_file_label.configure(text="Please select file", padx=68)
