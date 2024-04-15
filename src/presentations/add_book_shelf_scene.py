c92138da5a64e3ff10c0a81dfbac46451833a2a2
cesarjakub <cesar@spsejecna.cz>
2024-04-15 16:59:59 +0200

    feat: add book to shelf combo box

--- src/presentations/add_book_shelf_scene.py
+++ src/presentations/add_book_shelf_scene.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from CTkMessagebox import CTkMessagebox
 from src.data_access.daos.shelfDAO import ShelfDAO
+from src.data_access.daos.bookDAO import BookDAO
 from src.data_access.tables.shelf import Shelf
 from src.data_access.tables.book import Book
 
@@ -10,12 +11,14 @@ class AddBookShelfScene:
         self.logic = logic
         self.database = database
         self.root = ctk.CTk()
+        self.book_values = []
 
         ctk.set_default_color_theme("dark-blue")
         ctk.set_appearance_mode("Dark")
         self.root.title("Add book")
         self.root.geometry("400x275")
         self.root.resizable(False, False)
+        self.create_values()
         self.components()
 
     def components(self):
@@ -24,8 +27,9 @@ class AddBookShelfScene:
 
         self.title_label = ctk.CTkLabel(self.root, text="Enter title")
         self.title_label.grid(row=1, column=0, padx=(10, 5), pady=(5, 5))
-        self.title_input = ctk.CTkEntry(self.root, width=250, placeholder_text="Title...")
+        self.title_input = ctk.CTkComboBox(self.root, width=250, values=self.book_values)
         self.title_input.grid(row=1, column=1, padx=(5, 10), pady=(5, 5))
+        self.title_input.set("choose one")
 
         self.shelf_no_label = ctk.CTkLabel(self.root, text="Shelf number")
         self.shelf_no_label.grid(row=2, column=0, padx=(10, 5), pady=(5, 5))
@@ -39,6 +43,11 @@ class AddBookShelfScene:
 
         self.add_bk = ctk.CTkButton(self.root, text="Add book to shelf", command=self.add_book_shelf)
         self.add_bk.grid(row=4, column=0, columnspan=2, pady=50)
+
+    def create_values(self):
+        bkdao = BookDAO(self.database)
+        his_bk = bkdao.read()
+        self.book_values = [item[0] for item in his_bk]
 
     def check_for_input(self):
         if self.title_input.get() == "" or self.shelf_no_input.get() == "" or self.floor_input.get() == "":
@@ -76,7 +85,7 @@ class AddBookShelfScene:
             shelfdao.insert_record(shelf, book)
 
             CTkMessagebox(title="Success", message=f"Book {self.title_input.get()} added successfully!", icon="check")
-            self.title_input.delete(0, "end")
+            self.title_input.set("choose one")
             self.shelf_no_input.delete(0, "end")
             self.floor_input.delete(0, "end")
 
