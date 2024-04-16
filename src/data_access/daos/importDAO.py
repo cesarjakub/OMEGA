9c14f0a5046127ccf680a934c306a101ed2d99c8
cesarjakub <cesar@spsejecna.cz>
2024-04-16 17:52:09 +0200

    feat: radky!!!!

--- src/data_access/daos/importDAO.py
+++ src/data_access/daos/importDAO.py
@@ -19,13 +19,45 @@ class ImportDAO(IDao):
             raise Exception(e)
 
     def create(self, record):
-        pass
+        try:
+            msg = "Error with creating book."
+            query = """
+                    INSERT INTO book(Genre_ID, Author_ID, Title) VALUES(?,?,?)
+                    """
+            params = (record.genre_id, record.author_id, record.title,)
+            self.database.exec(query, params, msg)
+        except Exception as e:
+            raise Exception(e)
 
     def read(self):
-        pass
+        msg = "No records."
+        try:
+            query = """
+                            SELECT Title from book
+                            """
+            history = self.database.select(query, msg)
+            return history
+        except Exception as e:
+            return msg
 
     def update(self, record):
-        pass
+        try:
+            msg = "Error with updating book."
+            query = """
+                    UPDATE book SET Genre_ID = ?, Author_ID = ?, Title = ? WHERE ID = ?
+                    """
+            params = (record.genre_id, record.author_id, record.title, record.id,)
+            self.database.exec(query, params, msg)
+        except Exception as e:
+            raise Exception(e)
 
     def delete(self, record):
-        pass
+        try:
+            msg = "Error with deleting book."
+            query = """
+                    DELETE FROM book WHERE ID = ?
+                    """
+            params = (record.id,)
+            self.database.exec(query, params, msg)
+        except Exception as e:
+            raise Exception(e)
